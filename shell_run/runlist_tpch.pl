0cd3e66064aeca0610bffd9e74e3f6e725130dd2
umasou <392660318@qq.com>
2022-03-22 17:14:42 +0800

    add draw pic

--- shell_run/runlist_tpch.pl
+++ shell_run/runlist_tpch.pl
@@ -23,7 +23,7 @@ for my $query ( @queries ) {
 
 	print "Warming Query : $query\n"; 
 	my $warmStart = time();
-	my $cmd="(/home/ec2-user/trino/trino/bin/trino --server localhost:8001 --catalog hive -f ./$query) | tee  ../tpch_logs/$warn_logfile > /dev/null";
+	my $cmd="(/home/ec2-user/bigdata/trino/trino-server-370/bin/trino --server localhost:8001 --catalog hive -f ./$query) | tee  ../tpch_logs/$warn_logfile > /dev/null";
 	my @warnoutput=`$cmd`;
 
 	my $warmEnd = time();
@@ -32,38 +32,38 @@ for my $query ( @queries ) {
 	print LOG "$query,0 : $warmTime\n";
 
 } # end for
-
-# turn 1
-for my $query ( @queries ) {
-    my $warn_logfile = "$query.warn.log";
-    my $logname = "$query.log";
-	print "Running Query : $query\n";
-	my $runStart = time();
-	my $cmd2="(/home/ec2-user/trino/trino/bin/trino --server localhost:8001 --catalog hive -f ./$query) | tee  ../tpch_logs/$logname > /dev/null";
-	my @runoutput=`$cmd2`;
-
-	my $runEnd = time();
-	my $runTime = $runEnd - $runStart ;
-	print "$query Done in  $runTime secs\n";
-	print LOG "$query,1:  $runTime\n";
-
-} # end for
-
-# turn 2
-for my $query ( @queries ) {
-    my $warn_logfile = "$query.warn.log";
-    my $logname = "$query.log";
-    # turn 1
-	print "Running Query : $query\n";
-	my $runStart = time();
-	my $cmd2="(/home/ec2-user/trino/trino/bin/trino --server localhost:8001 --catalog hive -f ./$query) | tee  ../tpch_logs/$logname > /dev/null";
-	my @runoutput=`$cmd2`;
-
-	my $runEnd = time();
-	my $runTime = $runEnd - $runStart ;
-	print "$query Done in  $runTime secs\n";
-	print LOG "$query,2:  $runTime\n";
-
-} # end for
+#
+## turn 1
+#for my $query ( @queries ) {
+#    my $warn_logfile = "$query.warn.log";
+#    my $logname = "$query.log";
+#	print "Running Query : $query\n";
+#	my $runStart = time();
+#	my $cmd2="(/home/ec2-user/bigdata/trino/trino-server-370/bin/trino --server localhost:8001 --catalog hive -f ./$query) | tee  ../tpch_logs/$logname > /dev/null";
+#	my @runoutput=`$cmd2`;
+#
+#	my $runEnd = time();
+#	my $runTime = $runEnd - $runStart ;
+#	print "$query Done in  $runTime secs\n";
+#	print LOG "$query,1:  $runTime\n";
+#
+#} # end for
+#
+## turn 2
+#for my $query ( @queries ) {
+#    my $warn_logfile = "$query.warn.log";
+#    my $logname = "$query.log";
+#    # turn 1
+#	print "Running Query : $query\n";
+#	my $runStart = time();
+#	my $cmd2="(/home/ec2-user/bigdata/trino/trino-server-370/bin/trino --server localhost:8001 --catalog hive -f ./$query) | tee  ../tpch_logs/$logname > /dev/null";
+#	my @runoutput=`$cmd2`;
+#
+#	my $runEnd = time();
+#	my $runTime = $runEnd - $runStart ;
+#	print "$query Done in  $runTime secs\n";
+#	print LOG "$query,2:  $runTime\n";
+#
+#} # end for
 
 close LOG;
