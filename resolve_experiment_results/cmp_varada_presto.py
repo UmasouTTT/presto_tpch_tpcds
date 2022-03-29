7b91a263a98cdc84c73e6ffdf10474d793aab075
umasou <392660318@qq.com>
2022-03-29 19:17:11 +0800

    fix tpcds benchmark

--- resolve_experiment_results/cmp_varada_presto.py
+++ resolve_experiment_results/cmp_varada_presto.py
@@ -84,6 +84,7 @@ def analysis(results, exps):
             for exp in results[type][turn]:
                 whole_time += results[type][turn][exp]
             print("Whole time is {}".format(whole_time))
+            results[type][turn]["whole_time"] = whole_time
 
 def get_exps():
     exps = []
@@ -95,7 +96,6 @@ def get_exps():
                 for line in _f:
                     exps.append(line.strip().split(".")[0])
                 _f.close()
-
     return exps
 
 def draw_pic():
@@ -172,7 +172,58 @@ def draw_pic():
     plt.show()
 
 
-print(draw_pic())
+def draw_pic_for_whole_time():
+    # pic config
+    plt.figure(figsize=(13, 5))
+    bar_width = 15
+    color = ["red", "dodgerblue", "orange", "grey", "green"]
+
+    # draw
+    x = []
+
+    # parquet / orc
+    y = []
+
+    for i in range(len(y)):
+        start = i * 100
+        x = [start + j * bar_width for j in range(2)]
+        for _ in range(len(xs)):
+            xs[_].append(x[_])
+
+    for _ in range(len(xs)):
+        if "hive_0" == ys[_][0]:
+            plt.bar(xs[_], ys[_][1], width=bar_width, color=color[_], label="presto")
+        else:
+            plt.bar(xs[_], ys[_][1], width=bar_width, color=color[_], label=ys[_][0])
+
+    # 刻度
+
+    plt.xticks(x_ticks, exps, rotation=90)
+
+
+    # bins = np.linspace(-1, 1, 21)  # 横坐标起始和结束值，分割成21份
+    # plt.figure(figsize=(13, 5))  # 图像大小
+    # plt.xticks(bins)  # 设置x轴
+    # plt.xlim(-1, 1)  # x轴开始和结束位置
+
+    ax = plt.axes()
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+
+    # plt.title("Accelerate")
+
+    plt.ylabel("Completion time(sec)")
+
+    # plt.grid(axis="y", ls="--")
+    # 展示
+    # plt.legend( ncol=3, frameon=False, bbox_to_anchor=(0, 1))
+    # plt.legend('boxoff')
+    plt.legend(loc='upper center', ncol=5, frameon=False)
+
+
+    plt.show()
+
+
 
 
 
--- shell_run/runlist_tpcds.pl
+++ shell_run/runlist_tpcds.pl
@@ -20,54 +20,54 @@ chdir 'tpcds_varada';
 my @queries = glob '*.sql';
 
 #
-## warm
-print "***************************************Varada Warm**************************************************\n";
-for my $query ( @queries ) {
-	print "Warming Query : $query\n";
-	my $warmStart = time();
-	my $cmd="(/home/ec2-user/bigdata/trino/trino-server-370/bin/trino --server localhost:8080 --catalog varada -f ./$query)";
-	my @warnoutput=`$cmd`;
-
-	my $warmEnd = time();
-	my $warmTime = $warmEnd - $warmStart ;
-	print "Warmed Query : $query In $warmTime secs\n";
-	print VARADA_LOG "$query,0 : $warmTime\n";
-
-} # end for
+### warm
+#print "***************************************Varada Warm**************************************************\n";
+#for my $query ( @queries ) {
+#	print "Warming Query : $query\n";
+#	my $warmStart = time();
+#	my $cmd="(/home/ec2-user/bigdata/trino/trino-server-370/bin/trino --server localhost:8080 --catalog varada -f ./$query)";
+#	my @warnoutput=`$cmd`;
 #
-# turn one
-print "***************************************Varada Turn One**************************************************\n";
-for my $query ( @queries ) {
-
-	print "Turn one : $query\n";
-	my $warmStart = time();
-	my $cmd="(/home/ec2-user/bigdata/trino/trino-server-370/bin/trino --server localhost:8080 --catalog varada -f ./$query)";
-	my @warnoutput=`$cmd`;
-
-	my $warmEnd = time();
-	my $warmTime = $warmEnd - $warmStart ;
-	print "Warmed Query : $query In $warmTime secs\n";
-	print VARADA_LOG "$query,1 : $warmTime\n";
-
-} # end for
+#	my $warmEnd = time();
+#	my $warmTime = $warmEnd - $warmStart ;
+#	print "Warmed Query : $query In $warmTime secs\n";
+#	print VARADA_LOG "$query,0 : $warmTime\n";
 #
-# turn two
-print "***************************************Varada Turn Two**************************************************\n";
-for my $query ( @queries ) {
-
-	print "Warming Query : $query\n";
-	my $warmStart = time();
-	my $cmd="(/home/ec2-user/bigdata/trino/trino-server-370/bin/trino --server localhost:8080 --catalog varada -f ./$query)";
-	my @warnoutput=`$cmd`;
-
-	my $warmEnd = time();
-	my $warmTime = $warmEnd - $warmStart ;
-	print "Warmed Query : $query In $warmTime secs\n";
-	print VARADA_LOG "$query,2 : $warmTime\n";
-
-} # end for
-
-close VARADA_LOG;
+#} # end for
+##
+## turn one
+#print "***************************************Varada Turn One**************************************************\n";
+#for my $query ( @queries ) {
+#
+#	print "Turn one : $query\n";
+#	my $warmStart = time();
+#	my $cmd="(/home/ec2-user/bigdata/trino/trino-server-370/bin/trino --server localhost:8080 --catalog varada -f ./$query)";
+#	my @warnoutput=`$cmd`;
+#
+#	my $warmEnd = time();
+#	my $warmTime = $warmEnd - $warmStart ;
+#	print "Warmed Query : $query In $warmTime secs\n";
+#	print VARADA_LOG "$query,1 : $warmTime\n";
+#
+#} # end for
+##
+## turn two
+#print "***************************************Varada Turn Two**************************************************\n";
+#for my $query ( @queries ) {
+#
+#	print "Warming Query : $query\n";
+#	my $warmStart = time();
+#	my $cmd="(/home/ec2-user/bigdata/trino/trino-server-370/bin/trino --server localhost:8080 --catalog varada -f ./$query)";
+#	my @warnoutput=`$cmd`;
+#
+#	my $warmEnd = time();
+#	my $warmTime = $warmEnd - $warmStart ;
+#	print "Warmed Query : $query In $warmTime secs\n";
+#	print VARADA_LOG "$query,2 : $warmTime\n";
+#
+#} # end for
+#
+#close VARADA_LOG;
 #
 # test hive
 print "***************************************Hive Warm**************************************************\n";
